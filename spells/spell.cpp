0573b8ed353fa34e6b58424545f1ef3068d3b0b2
gymfox <solod.andrew93@gmail.com>
2017-06-20 23:07:49 +0300

    Implemented spellbooks. Corrected takeMagicDamage methods. Adding Spells and etc. Adding Priest class.

--- spells/spell.cpp
+++ spells/spell.cpp
@@ -1,6 +1,6 @@
 #include "spell.h"
 
-Spell::Spell(const string& name, int manaConsumption, int hitPoints) {}
+Spell::Spell() {}
 
 Spell::~Spell() {}
 
@@ -14,4 +14,24 @@ int Spell::getManaConsumption() const {
 
 int Spell::getHitPoints() const {
     return hitPoints;
+}
+
+int Spell::getSpellsType() const {
+    return spellsType;
+}
+
+void Spell::setSpellsName(const string& newSpellsName) {
+    spellsName = newSpellsName;
+}
+
+void Spell::setManaConsumption(int newManaConsumption) {
+    manaConsumption = newManaConsumption;
+}
+
+void Spell::setHitPoints(int newHitPoints) {
+    hitPoints = newHitPoints;
+}
+
+void Spell::setSpellsType(int newSpellsType) {
+    spellsType = newSpellsType;
 }
--- spells/spell.h
+++ spells/spell.h
@@ -1,26 +1,39 @@
 #ifndef SPELL_H
 #define SPELL_H 
 
-#include "../mystic/spellcaster.h"
+#include "../units/unit.h"
 
 using namespace std;
 
 class Spellcaster;
 class Ability;
+
+enum SpellsType {
+    battleSpell,
+    healSpell
+};
     
 class Spell {
     private:
         string spellsName;
         int manaConsumption;
         int hitPoints;
+        int spellsType;
+
+    protected:
+        virtual void setSpellsName(const string& newSpellsName);
+        virtual void setManaConsumption(int newManaConsumption);
+        virtual void setHitPoints(int newHitPoints);
+        virtual void setSpellsType(int newSpellsType);
         
     public:
-        Spell(const string& name, int manaConsumption, int hitPoints);
+        Spell();
         virtual ~Spell();
     
         virtual const string& getSpellsName() const;
         virtual int getManaConsumption() const;
         virtual int getHitPoints() const;
+        virtual int getSpellsType() const;
 };
 
 
